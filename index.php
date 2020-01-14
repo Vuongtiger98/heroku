37f5ba311c7168283b53de433de45a497ecd856f
Vuongtiger98 <59682578+Vuongtiger98@users.noreply.github.com>
2020-01-14 16:16:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,8 @@
   </head>
   <body
     <?php echo '<p>Hello World</p>'; ?>
+    <img src="trump.jpg" alt="Trump">
+    <a href="google.com">Google</a>
+  <a href="profile.html">Facebook</a>
   </body
 </html
