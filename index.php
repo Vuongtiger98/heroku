e6eb40c394ae3accb026e80458bc6080f2521203
Vuongtiger98 <59682578+Vuongtiger98@users.noreply.github.com>
2020-01-14 16:24:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
     <title> </title>
   </head>
   <body
-    <?php echo '<p>Hello World</p>'; ?>
-    <img src="trump.jpg" alt="Trump">
-    <a href="google.com">Google</a>
-  <a href="profile.html">Facebook</a>
+    <?php echo '<p>Hello World</p>'; ?><br>
+    <img src="trump.jpg" alt="Trump"><br>
+    <a href="http://www.google.com">Google</a><br>
+  <a href="profile.html">Facebook</a><br>
   </body
 </html
